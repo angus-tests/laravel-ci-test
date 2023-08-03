f7dabe1deeb78429d27e3ae5c8985823632ee0d3
Angus <angus.goody@gmail.com>
2023-08-03 15:24:35 +0100

    using sqlite

--- prod.sh
+++ prod.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+# create database
+touch db.sqlite
+
 # Generate .env file
 echo "APP_NAME=${APP_NAME}" > .env
 echo "APP_ENV=${APP_ENV}" >> .env
-echo "DB_CONNECTION=${DB_CONNECTION}" >> .env
-echo "DB_HOST=${DB_HOST}" >> .env
-echo "DB_PORT=${DB_PORT}" >> .env
-echo "DB_DATABASE=${DB_DATABASE}" >> .env
-echo "DB_USERNAME=${DB_USERNAME}" >> .env
-echo "DB_PASSWORD=${DB_PASSWORD}" >> .env
+echo "DB_CONNECTION=sqlite" >> .env
+echo "DB_DATABASE=db.sqlite">> .env
+echo "DB_FOREIGN_KEYS=true" >> .env
+#echo "DB_HOST=${DB_HOST}" >> .env
+#echo "DB_PORT=${DB_PORT}" >> .env
+#echo "DB_USERNAME=${DB_USERNAME}" >> .env
+#echo "DB_PASSWORD=${DB_PASSWORD}" >> .env
+
+
 
 # Run artisan commands
 php artisan storage:link
