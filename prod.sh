a7eedcd830e470406042be61995ab9b3f673e7dc
Angus <angus.goody@gmail.com>
2023-08-03 15:25:37 +0100

    removed old ones

--- prod.sh
+++ prod.sh
@@ -9,11 +9,6 @@ echo "APP_ENV=${APP_ENV}" >> .env
 echo "DB_CONNECTION=sqlite" >> .env
 echo "DB_DATABASE=db.sqlite">> .env
 echo "DB_FOREIGN_KEYS=true" >> .env
-#echo "DB_HOST=${DB_HOST}" >> .env
-#echo "DB_PORT=${DB_PORT}" >> .env
-#echo "DB_USERNAME=${DB_USERNAME}" >> .env
-#echo "DB_PASSWORD=${DB_PASSWORD}" >> .env
-
 
 
 # Run artisan commands
