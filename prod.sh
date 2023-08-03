1ad1ed21b74a0e38ea7fbfd4c576fea84e3da00a
Angus <angus.goody@gmail.com>
2023-08-03 17:27:45 +0100

    fixed bug

--- prod.sh
+++ prod.sh
@@ -3,10 +3,10 @@
 # Generate .env file
 
 
-echo "APP_NAME=${APP_NAME}" >> .env
+echo "APP_NAME=${APP_NAME}" > .env
 echo "APP_URL=${APP_URL}" >> .env
 echo "APP_ENV=${APP_ENV}" >> .env
-echo "APP_KEY=${APP_KEY}" > .env
+echo "APP_KEY=${APP_KEY}" >> .env
 echo "DB_CONNECTION=${DB_CONNECTION}" >> .env
 echo "DB_HOST=${DB_HOST}" >> .env
 echo "DB_PORT=${DB_PORT}" >> .env
